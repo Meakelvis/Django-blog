aceeeaf24491eecca34eab3d4e7563a48057a7c6
Meakelvis <markelvis37@gmail.com>
2019-07-29 22:10:33 +0300

    Settings update

--- django_project/settings.py
+++ django_project/settings.py
@@ -133,6 +133,6 @@ LOGIN_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = turned
+# EMAIL_USE_TLS = turned
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
